71baf43338bb864722a6c673dcb64c9f0898d59e
Dante Benicio <dantecosta79@gmail.com>
2022-11-21 15:53:17 -0300

    feat(component): main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Orders from './components/Orders';
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <ToastContainer position='bottom-center' />
+      <Header />
+      <Orders />
+    </>
+  );
 }
