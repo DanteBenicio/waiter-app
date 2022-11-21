bf5f3a4966079487e57746ee5ef119c6dd540f5c
Dante Benicio <dantecosta79@gmail.com>
2022-11-21 15:59:58 -0300

    feat(component): main component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>
-)
+  </>
+);
